bff954fe5951ed8cc2d2a69fa486470f9d6f9784
justnoserena <justnoserena@gmail.com>
2022-08-25 18:42:42 -0700

    Updated Code

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# E-commerce Back End
- AS A manager at an internet retail company I WANT a back end for my e-commerce website that uses the latest technologies SO THAT my company can compete with other e-commerce companies
+# E-Commerce-Back-End
+  ## Description
+  ---
+  As A manager at an internet retail company, I want a back end for my e-commerce website that use the latest technologies, SO THAT my company can compete with other e-commerce companies
+  ## Table of Contents
+  ---
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [Questions](#questions)
+
+  ## Installation
+  ---
+  
+  ## Usage
+  ![Deployed Application video](https://drive.google.com/file/d/11ji0J1oDWUyFgtmAJA9CLMT_BaHngfyI/view)
+  [Application Link]()
+
+  GIVEN a functional Express.js API
+WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands
+THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application
+THEN my server is started and the Sequelize models are synced to the MySQL database
+WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+THEN I am able to successfully create, update, and delete data in my database
+  
+  ## Credits
+  ---
+  [undefined]
+  ## Questions
+  ---
+  Github Username: Justnoserena
+  GitHub Repository: [Repository Link]{}
+  Email: Justnoserena@gmail.com
+
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // find all tags
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(libraryCardData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create({
+      
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  // create a new tag
+});
+
+router.put('/:id',async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+  // delete on tag by its `id` value
+
+module.exports = router;
